4fa2205311700fa7a1494d2d6a1ddf33386175dc
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-14 17:23:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,6 @@ def create_project_hash
   project.css("div.project-thumbnail a img").attibute("src").value
   project.css("p.bbcard_blurb").text
   project.css("ul.project-meta span.location-name").text
-  project.css("ul.project-stats li.first.funded strong").text
+  project.css("ul.project-stats li.first.funded strong").text.text.gsub("%","").to_i
   
 end
