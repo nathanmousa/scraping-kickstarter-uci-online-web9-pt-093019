23b62334e342c483df3bcf675c0203bccc02f9c9
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-14 17:23:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,8 +4,12 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  projects = {}
   
   kickstarter.css("li.project.grid_4")
+  
+  
+  
   project.css("h2.bbcard_name strong a").text
   project.css("div.project-thumbnail a img").attibute("src").value
   project.css("p.bbcard_blurb").text
